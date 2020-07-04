ef6f59dfdce48e2d6db29a10a7d4171ad1b8713f
Dave Thornton <davethornton@qatest.co.uk>
2020-07-04 10:29:07 +0100

    Create settings.sh

---
+++ settings.sh
@@ -0,0 +1,10 @@
+PRODUCT="Programatics"
+SHORTCUT="pgm"
+VERSION="1.0"
+GITREPO="https://github.com/qaTestLimited/Programatics.git"
+POSTGRES_USER="postgres"
+POSTGRES_PSWD="postgres"
+RETRIES=30 #number of retries / seconds to wait for postgres to start
+DBUSER="devpgm"
+DBNAME="devpgm"
+DBPSWD="d1e2v3p4g5m6"
